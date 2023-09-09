4d6ac050461b138688d9562098dc54757278aedb
weapyy <arewordswords@gmail.com>
2023-09-09 13:45:52 +0800

    CREATE COURT

---
+++ webpage/court.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, intial-scale=1.0">
+    <title>Pro One Badminton Center - COURT</title>
+    <link rel="stylesheet" href="css/common.css">
+    <?php require('inc/links.php'); ?>
+    <style>
+        .pop:hover{
+            border-top-color: var(--teal_hover) !important;
+            transform: scale(1.03);
+            transition: all 0.3s;
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+
+<?php require('inc/header.php');?>
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">OUR COURT</h2> 
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium omnis modi, sint 
+            vitae minus et debitis totam provident obcaecati nemo dolorum tempore esse perferendis 
+            nostrum nulla asperiores reprehenderit sapiente facilis!
+        </p>
+    </div>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 cl-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-4">
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus, eaque 
+                        vitae quae nobis fuga consequuntur, ratione nostrum hic magnam iure, incidunt odit provident nemo 
+                        enim praesentium quo debitis optio.
+
+                    </p>
+                </div>
+            </div><div class="col-lg-4 cl-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-4">
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus, eaque 
+                        vitae quae nobis fuga consequuntur, ratione nostrum hic magnam iure, incidunt odit provident nemo 
+                        enim praesentium quo debitis optio.
+
+                    </p>
+                </div>
+            </div><div class="col-lg-4 cl-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-4">
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus, eaque 
+                        vitae quae nobis fuga consequuntur, ratione nostrum hic magnam iure, incidunt odit provident nemo 
+                        enim praesentium quo debitis optio.
+
+                    </p>
+                </div>
+            </div><div class="col-lg-4 cl-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-4">
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus, eaque 
+                        vitae quae nobis fuga consequuntur, ratione nostrum hic magnam iure, incidunt odit provident nemo 
+                        enim praesentium quo debitis optio.
+
+                    </p>
+                </div>
+            </div><div class="col-lg-4 cl-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-4">
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus, eaque 
+                        vitae quae nobis fuga consequuntur, ratione nostrum hic magnam iure, incidunt odit provident nemo 
+                        enim praesentium quo debitis optio.
+
+                    </p>
+                </div>
+            </div><div class="col-lg-4 cl-md-6 mb-5 px-4">
+                <div class="bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-4">
+                        <img src="images/features/wifi.svg" width="40px">
+                        <h5 class="m-0 ms-3">Wifi</h5>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ducimus, eaque 
+                        vitae quae nobis fuga consequuntur, ratione nostrum hic magnam iure, incidunt odit provident nemo 
+                        enim praesentium quo debitis optio.
+
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php require('inc/footer.php') ?>
+
+</body>
+</html>
