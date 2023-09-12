cb476ffb2f8f6c6b62d3f82004ea5a5b4f866f13
David Goh <141190747+DavidGohtw@users.noreply.github.com>
2023-09-12 12:58:14 +0800

    db connect updated

--- AdminDashboard/dbconnect.php
+++ AdminDashboard/dbconnect.php
@@ -4,16 +4,17 @@ $dbname = "proonebadmintoncentre"; // Change to your database name
 $username = "proadmin38"; // Change to your database username
 $password = "proadmin38"; // Change to your database password
 
- /* try {
+  try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn=mysqli_connect("localhost","proadmin38","proadmin38","proonebadmintoncentre");
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
-} */
+} 
 
-$conn=mysqli_connect("localhost","proadmin38","proadmin38","proonebadmintoncentre");
+/* $conn=mysqli_connect("localhost","proadmin38","proadmin38","proonebadmintoncentre");
 
 if(!$conn){
     die("Connection failed: " . mysqli_connect_error());
-}
+} */
 ?>
