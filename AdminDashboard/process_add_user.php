5f5c7eee573823af0eb16e7730e41e27bd08ec3f
ilhankamil <122461279+ilhankamil@users.noreply.github.com>
2023-10-11 03:29:42 +0800

    UPDATED ADD USER

--- AdminDashboard/process_add_user.php
+++ AdminDashboard/process_add_user.php
@@ -13,24 +13,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Insert the new user into the database
         $stmt = $pdo->prepare("INSERT INTO user (username, email, password, phonenumber, userType) VALUES (:username, :email, :password, :phonenumber, :userType)");
 
+        // Validate and sanitize user inputs before binding parameters
+        // Example: $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        // You should apply similar validation and sanitization to other inputs
+
         $stmt->bindParam(':username', $username, PDO::PARAM_STR);
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
         $stmt->bindParam(':password', $password, PDO::PARAM_STR);
         $stmt->bindParam(':phonenumber', $phonenumber, PDO::PARAM_STR);
         $stmt->bindParam(':userType', $userType, PDO::PARAM_STR);
 
-if ($stmt->execute()) {
-    // User added successfully, update verified_email and verification_code
-    $user_id = $pdo->lastInsertId(); // Get the ID of the newly added user
+        if ($stmt->execute()) {
+            // User added successfully, update verified_email and verification_code
+            $user_id = $pdo->lastInsertId(); // Get the ID of the newly added user
 
-    $stmt = $pdo->prepare("UPDATE user SET verified_email = 1, verification_code = '000000' WHERE id = :id");
-    $stmt->bindParam(':id', $user_id, PDO::PARAM_INT);
+            $stmt = $pdo->prepare("UPDATE user SET verified_email = 'verified', verification_code = '000000' WHERE id = :id");
+            $stmt->bindParam(':id', $user_id, PDO::PARAM_INT);
 
-    if ($stmt->execute()) {
-        // Verification fields updated successfully
-        header("Location: usermanage.php"); // Redirect back to the user management page
-        exit();
-    } else {
+            if ($stmt->execute()) {
+                // Verification fields updated successfully
+                header("Location: usermanage.php"); // Redirect back to the user management page
+                exit();
+            } else {
+                // Error handling (e.g., display an error message)
+                echo "Error updating user verification status.";
+            }
+        } else {
             // Error handling (e.g., display an error message)
             echo "Error adding user.";
         }
