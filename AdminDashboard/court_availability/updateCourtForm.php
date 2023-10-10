7488f203baf09a73748a96e938d34623c07de671
weapyy <arewordswords@gmail.com>
2023-10-11 00:35:57 +0800

    CREATE UPDATE COURT AVAILABILITY FORM

--- AdminDashboard/court_availability/updateCourtForm.php
+++ AdminDashboard/court_availability/updateCourtForm.php
@@ -1,61 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Court Avalability Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-
 <body>
 <?php
-
 include "courtFunction.php";
-//print_r($_POST);
 
-$courtid=$_POST['courtIdToUpdate'];
-
+$courtid = $_POST['courtIdToUpdate'];
 $courtQry = getCourtInformation($courtid);
-echo "No. of record:".mysqli_num_rows($courtQry);
-$courtInfo=mysqli_fetch_assoc($courtQry);
-$courtName=$courtInfo['courtName'];
-$courtStatus=$courtInfo['courtStatus'];
 
-/*echo '<form action="courtFunction.php" method="POST">';
-    echo" Court ID: <input type='text' name='courtid' value='$courtid' readonly>";
-    echo"<br>Court Name: <input type='text' name='courtName' value='$courtName'>";
-    echo "<select name='courtStatus'>";
-        echo "<option value='$courtStatus'>Available</option>";
-        echo "<option value='$courtStatus'>Booked</option>";
-        echo "</select>";
-
-    echo '<input type="submit" name="saveCourtButton" value="save">';
-
-
-echo '</form>';*/
-
-
-
-echo '<div class="container">';
-echo'<div class="row">';
-echo'<div class="col">';
-echo'<form action="courtFunction.php" method="POST">';
-
-echo'<label for="updateCourtId">Court ID</label>';
-echo"<input type='text' class='form-control' name='courtid' value='$courtid' placeholder='Enter Court ID' readonly>";
-
-
-echo'<label for="updateCourtName">Court Name</label>';
-echo"<input type='text' class='form-control' name='courtName' value='$courtName' placeholder='Enter Court name'>";
-
-echo'<label for="inputCourtStatus">Status</label>';
-echo"<select name='courtStatus' id='courtStatus' class='form-control'>";
-echo'<option value="$courtStatus">Available</option>';
-echo'<option value="$courtStatus">Booked</option>';
-echo'</select><br>';
-
-        echo'<div class="mx-auto" style="width: 200px;">';
-        echo"<button type='submit' class='btn btn-primary' name='saveCourtButton' value='save'>Submit</button>";
-        echo'</div>';
-
+$courtInfo = mysqli_fetch_assoc($courtQry);
+$courtName = $courtInfo['courtName'];
+$courtStatus = $courtInfo['courtStatus'];
 ?>
+<div class="container mt-5">
+    <h2 class="text-center">Update Court Information</h2>
+            <form action="courtProcess.php" method="POST">
+            <div class="form-group">
+                <label for="updateCourtId">Court ID</label>
+                <input type="text" class="form-control" name="courtid" value="<?php echo $courtid; ?>" placeholder="Enter Court ID" readonly>
+            </div>
+
+            <div class="form-group">
+                <label for="updateCourtName">Court Name</label>
+                <input type="text" class="form-control" name="courtName" value="<?php echo $courtName; ?>" placeholder="Enter Court name">
+            </div>
+
+            <div class="form-group">
+                <label for="inputCourtStatus">Status</label>
+                <select name="courtStatus" id="courtStatus" class="form-control">
+                    <option value="Available" <?php echo ($courtStatus == "Available") ? 'selected' : ''; ?>>Available</option>
+                    <option value="Booked" <?php echo ($courtStatus == "Booked") ? 'selected' : ''; ?>>Booked</option>
+                </select><br>
+            </div>
+
+                    <button type="submit" class="btn btn-primary" name="saveCourtButton" value="save">Submit</button>
+            </form>
+            <a href="courtList.php" class="mt-3 btn btn-secondary">Back to Court Availability</a>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
