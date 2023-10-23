82bd53a9199722511cd88dfa3d62f14c975c5bc6
ilhankamil <122461279+ilhankamil@users.noreply.github.com>
2023-10-24 02:17:50 +0800

    cleaner table for court rate

--- Customer/backup_index.php
+++ Customer/backup_index.php
@@ -1,60 +1,22 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <title>Pro One Badminton Center</title>
     <link rel="stylesheet" href="css/common.css">
     <?php require('inc/links.php'); ?>
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
-    <style>
-        /* Style for the table container */
-        .table-container {
-            max-width: 800px;
-            margin: 0 auto;
-            background-color: #f7f7f7;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-            padding: 15px;
-            border: 1px solid #ddd;
-        }
-
-        /* Style for the table */
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-
-        /* Style for table headers */
-        th {
-            background-color: #333;
-            color: #fff;
-            border: 1px solid #000;
-            padding: 10px;
-            text-align: center;
-        }
-
-        /* Style for table cells */
-        td {
-            text-align: center;
-            border: 1px solid #000;
-            padding: 10px;
-        }
-
-        /* Add some spacing for better readability */
-        .table-container h1 {
-            margin-bottom: 20px;
-            text-align: center;
-        }
-    </style>
 </head>
 
 <body class="bg-light">
@@ -136,32 +98,64 @@
             ?>
                 <br>
 
+                <style>
+        /* Style for the table container */
+        .table {
+            width: 400px; /* Set the width to create a square */
+            height: 200px; /* Set the height to create a square */
+            margin: 0 auto; /* Center the container horizontally */
+            background-color: #f7f7f7; /* Background color for the shadow */
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); /* Add a shadow effect */
+            padding: 5px; /* Add padding for content */
+            overflow-y: auto; /* Add vertical scroll if content overflows */
+            border: 1px solid #000; /* Add a border around the container */
+        }
+
+        /* Style for the table */
+        .table table {
+            width: 100%;
+            border-collapse: collapse; /* Collapse the table borders */
+        }
+
+        /* Style for table headers */
+        .table th {
+            background-color: #333; /* Header background color */
+            color: #fff; /* Header text color */
+            border: 1px solid #000; /* Add a border around header cells */
+            padding: 8px; /* Add padding to header cells */
+        }
+
+        /* Style for table cells */
+        .table td {
+            text-align: center; /* Center-align cell content */
+            border: 1px solid #000; /* Add a border around data cells */
+            padding: 8px; /* Add padding to data cells */
+        }
+    </style>
 
 
 
                 <!-- Begin Page Content -->
- <div class="container-fluid">
-        <div class="table-container">
-            <h1 class="h3 mb-4 text-gray-800">Court Rates</h1>
-            <!-- Display court rates in a table -->
-            <table>
-                <thead>
-                    <tr>
-                        <th>Time Slot</th>
-                        <th>Rate</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($courtRates as $rate) { ?>
+                <div class="container-fluid">
+                 <h1 class="h3 mb-4 text-gray-800" style="text-align: center;">Court Rates</h1>
+            
+                 <!-- Display court rates in a table or styled format -->
+                    <table class="table">
+                        <thead>
+                             <tr>
+                                <th>Time Slot</th>
+                                <th>Rate</th>
+                            </tr>
+                        </thead>
+                     <tbody>
+                        <?php foreach ($courtRates as $rate) { ?>
                         <tr>
                             <td><?php echo $rate['dayOfWeek']; ?></td>
                             <td><?php echo $rate['rate']; ?></td>
                         </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
+                            <?php } ?>
+                     </tbody>
+                    </table>
 <br><br><br>
 
     
