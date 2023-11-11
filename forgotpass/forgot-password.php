3d8329b71bcaf211105f57141c6e88a084de31cd
ilhankamil <122461279+ilhankamil@users.noreply.github.com>
2023-11-11 17:32:41 +0800

    updated interface for forgot password

--- forgotpass/forgot-password.php
+++ forgotpass/forgot-password.php
@@ -1,22 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Forgot Password</title>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="author" content="Yinka Enoch Adedokun">
+    <meta name="description" content="Simple Forgot Password Page Using HTML and CSS">
+    <meta name="keywords" content="forgot password page, basic html and css">
+    <title>Forgot Password Page - HTML + CSS</title>
+    <link rel="stylesheet" href="thestyle.css">
 </head>
 <body>
-
-    <h1>Forgot Password</h1>
+ <div class="row">
+        <h1>Forgot Password</h1>
+        <h6 class="information-text">Enter your registered email to reset your password.</h6>
 
     <form method="post" action="send-password-reset.php">
-
-        <label for="email">email</label>
+            <div class="form-group">
+        <label for="email">Email</label>
         <input type="email" name="email" id="email">
 
-        <button>Send</button>
+        <button onclick="showSpinner()">Reset Password</button>
 
-    </form>
+            </div>
+        </form>
 
 </body> <!-- Forgotpassword page -->
 </html>
